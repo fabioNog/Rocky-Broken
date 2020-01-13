23e71e8314b5d5c9ea68643779a4e67c4b3e77d8
fabioNog <fabio.carmo@computacao.ufla.br>
2020-01-13 11:44:17 -0300

    Create and Insert the components, TodoList,Create-Todo and Edit_Todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+
+//import of the components
+
+import TodoList from './components/todo-list.component'
+import EditTodo from './components/edit-todo.component'
+import CreateTodo from './components/create-todo.component'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <h1>DevNozes APP</h1>
+      </div>
+      <Route path="/" exact component={TodoList}/>
+      <Route path="/edit/:id" component={EditTodo}/>
+      <Route path="/create" component={CreateTodo}/>
+    </Router>
   );
 }
 
---
+++ src/components/create-todo.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateTodo extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Welcome Create Todo </h1>
+            </div>
+        )
+    }
+}
